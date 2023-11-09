8f01f18e7886670161be0481df8f63d4fc5b2e66
HectorDanielAyarachiFuentes <135895299+HectorDanielAyarachiFuentes@users.noreply.github.com>
2023-11-09 14:21:41 -0300

    456

---
+++ PartesWeb/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <p>© {new Date().getFullYear()} Mi Sitio Web</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
